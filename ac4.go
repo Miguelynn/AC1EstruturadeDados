8c0b8c7381ce2a6b73222b0081bb41e88038159f
Miguelynn <141842885+Miguelynn@users.noreply.github.com>
2023-09-01 10:53:18 -0300

    Update ac4.go

--- ac4.go
+++ ac4.go
@@ -1,1 +1,26 @@
+package main
 
+import (
+	"fmt"
+)
+
+func hanoi(n int, A string, B string, C string) int {
+	if n == 1 {
+		fmt.Printf("Mova o disco 1 de %s para %s\n", A, C)
+		return 1
+	}
+
+	movimentos := hanoi(n-1, A, C, B)
+
+	fmt.Printf("Mova o disco %d de %s para %s\n", n, A, C)
+
+	movimentos += hanoi(n-1, B, A, C)
+
+	return movimentos
+}
+
+func main() {
+	n := 3 
+	movimentos := hanoi(n, "A", "B", "C")
+	fmt.Printf("Número de movimentos necessários: %d\n", movimentos)
+}
